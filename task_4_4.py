0973bef262ce3ecd14a56ae3f5a427cc7eb519ad
Alina-11111 <alina.grib1323@gmail.com>
2026-05-09 12:49:24 +0300

    task4_4 while

--- task_4_4.py
+++ task_4_4.py
@@ -1,7 +1,19 @@
+#for
 numb = [1, 2, 3, 4, 5]
 new_numb = []
 for i in range(1, len(numb)):
     new_numb.append(numb[i])
+if len(new_numb) > 0:
+    new_numb.append(numb[0])
+print(new_numb)
+
+#while
+numb = [1, 2, 3, 4, 5]
+new_numb = []
+i = 1
+while i <len(numb):
+    new_numb.append(numb[i])
+    i +=1
 if len(new_numb) > 0:
     new_numb.append(numb[0])
 print(new_numb)
